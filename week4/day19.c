2ace3c7c25c80393b58c0907e3d5132aea8c2d7e
hoddukzoa <jinukim123@naver.com>
2024-12-30 23:49:17 +0900

    week4 day19

---
+++ week4/day19.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+long long int fibo(int n){
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+
+    return fibo(n-1) + fibo(n-2);
+}
+
+int main(){
+    long long int n;
+
+    scanf("%d", &n);
+
+    printf("%d\n", fibo(n));
+
+    return 0;
+
+}
